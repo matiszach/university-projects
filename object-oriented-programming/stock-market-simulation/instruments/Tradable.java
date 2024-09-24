738cf0c717f07978345a788cd94d1d9eb9d1c27c
Mateusz Kowalski <mjakub@Mateuszs-MacBook-Air.local>
2024-09-24 10:50:10 +0200

    update

--- object-oriented-programming/stock-market-simulation/instruments/Tradable.java
+++
@@ -1,8 +0,0 @@
-package instruments;
-
-public interface Tradable {
-    public String getTicker();
-    public double getSma(int from, int k);
-    public int getLastPrice();
-    public void setLastPrice(int price, int turn);
-}
